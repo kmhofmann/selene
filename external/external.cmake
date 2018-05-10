e66b7dae682c5f5270d70fb3dbc8f64e9c773242
Michael Hofmann <kmhofmann@gmail.com>
2018-05-10 13:07:03 +0200

    Only update Catch2 submodule when building tests.

--- external/external.cmake
+++ external/external.cmake
@@ -1,5 +1,7 @@
 # Install external libraries
 
 # Catch
-message(STATUS "Installing Catch submodule")
-execute_process(COMMAND git submodule update --init -- external/Catch2 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+if (SELENE_BUILD_TESTS)
+    message(STATUS "Installing Catch submodule")
+    execute_process(COMMAND git submodule update --init -- external/Catch2 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+endif()
