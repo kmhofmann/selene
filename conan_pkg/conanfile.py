709d7afa056668872921001b65cd41cb0531fdf6
Michael Hofmann <kmhofmann@gmail.com>
2018-05-10 14:56:01 +0200

    Added conanfile.py recipe.
    Also minor restructuring of CMake code.

---
+++ conan_pkg/conanfile.py
@@ -0,0 +1,33 @@
+import os
+from conans import ConanFile, CMake
+
+class SeleneConan(ConanFile):
+    name = "selene"
+    version = "0.1.0"
+    license = "MIT"
+    url = "https://github.com/kmhofmann/selene"
+    description = "A C++14 image representation, processing and I/O library. "
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    build_policy = "missing"
+
+    requires = ("libjpeg-turbo/[>1.5.0]@bincrafters/stable",
+                "libpng/[>1.2.0]@bincrafters/stable")
+
+    exports_sources = ("../selene*",
+                       "../cmake*",
+                       "../CMakeLists.txt",
+                       "../conanfile.txt",
+                       "../LICENSE")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="")
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["selene_base", "selene_img", "selene_io"]
---
+++ conan_pkg/test_package/example.cpp
@@ -0,0 +1,10 @@
+#include "selene/base/Allocators.hpp"
+#include "selene/img/Image.hpp"
+
+int main() {
+  using namespace sln::literals;
+  sln::NewAllocator::allocate(256);
+  sln::Image_8u1 img(20_px, 20_px);
+
+  return 0;
+}
