bc673d32c009b169dda03323d36c34aed2fd86ab
Michael Hofmann <kmhofmann@gmail.com>
2019-01-13 16:12:35 +0100

    Change way dependencies are handled with CMake options.
    Clearer CMake status messages.
    Add "base" CI test, without any dependencies installed.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -1,13 +1,18 @@
 # Centralized dependency handling
 
+set(selene_fp_failure_msg_user " (requested by user)")
+set(selene_fp_failure_msg_nf " (*** NOT FOUND ***)")
+
 # libjpeg-turbo (or libjpeg)
 
-if (NOT SELENE_NO_LIBJPEG)
-    find_package(JPEG)
+set(selene_fp_failure_msg ${selene_fp_failure_msg_user})
+if (SELENE_WITH_LIBJPEG)
+    find_package(JPEG QUIET)
+    set(selene_fp_failure_msg ${selene_fp_failure_msg_nf})
 endif()
 
 if (JPEG_FOUND)
-    message(STATUS "Building with libjpeg support.")
+    message(STATUS "Building with libjpeg support (${JPEG_LIBRARY}; version ${JPEG_VERSION}).")
     # Detect some features provided by libjpeg-turbo: partial image decoding and extended colorspaces
     set(JPEG_PARTIAL_DECODING OFF)
     set(JPEG_EXTENDED_COLORSPACES OFF)
@@ -26,35 +31,40 @@ if (JPEG_FOUND)
         unset(jpeg_supports_jcs_ext)
     endif()
     if (JPEG_PARTIAL_DECODING)
-        message(STATUS "libjpeg-turbo supports partial decoding.")
+        message(STATUS "  libjpeg-turbo supports partial decoding.")
     endif()
     if (JPEG_EXTENDED_COLORSPACES)
-        message(STATUS "libjpeg-turbo supports extended color spaces.")
+        message(STATUS "  libjpeg-turbo supports extended color spaces.")
     endif()
 else()
-    message(STATUS "Building without libjpeg support.")
+    message(STATUS "Building WITHOUT libjpeg support${selene_fp_failure_msg}.")
 endif()
 
 # libpng
 
-if (NOT SELENE_NO_LIBPNG)
-    find_package(PNG)
+set(selene_fp_failure_msg ${selene_fp_failure_msg_user})
+if (SELENE_WITH_LIBPNG)
+    find_package(PNG QUIET)
+    set(selene_fp_failure_msg ${selene_fp_failure_msg_nf})
 endif()
 
 if (PNG_FOUND)
-    message(STATUS "Building with libpng support.")
+    message(STATUS "Building with libpng support (${PNG_LIBRARY}; version ${PNG_VERSION_STRING}).")
 else()
-    message(STATUS "Building without libpng support.")
+    message(STATUS "Building WITHOUT libpng support${selene_fp_failure_msg}.")
 endif()
 
 # libtiff
 
-if (NOT SELENE_NO_LIBTIFF)
-    find_package(TIFF)
+set(selene_fp_failure_msg ${selene_fp_failure_msg_user})
+if (SELENE_WITH_LIBTIFF)
+    find_package(TIFF QUIET)
+    set(selene_fp_failure_msg ${selene_fp_failure_msg_nf})
 endif()
 
 if (TIFF_FOUND)
-    message(STATUS "Building with libtiff support.")
+    message(STATUS "Building with libtiff support (${TIFF_LIBRARY}; version ${TIFF_VERSION_STRING}).")
+
     # Detect some features provided only by recent libtiff versions (v4.0.10 and up)
     set(TIFF_SUPPORT_ZSTD_AND_WEBP OFF)
     if (TIFF_INCLUDE_DIR AND EXISTS "${TIFF_INCLUDE_DIR}/tiff.h")
@@ -67,21 +77,23 @@ if (TIFF_FOUND)
         unset(tiff_supports_webp_compression)
     endif()
     if (TIFF_SUPPORT_ZSTD_AND_WEBP)
-        message(STATUS "libtiff supports Zstd and WebP (de)compression, with respective libraries present.")
+        message(STATUS "  libtiff supports Zstd and WebP (de)compression, if respective libraries are present.")
     endif()
 else()
-    message(STATUS "Building without libtiff support.")
+    message(STATUS "Building WITHOUT libtiff support${selene_fp_failure_msg}.")
 endif()
 
 # OpenCV
 
-if (NOT SELENE_NO_OPENCV)
-    find_package(OpenCV)
+set(selene_fp_failure_msg ${selene_fp_failure_msg_user})
+if (SELENE_WITH_OPENCV)
+    find_package(OpenCV QUIET)
+    set(selene_fp_failure_msg ${selene_fp_failure_msg_nf})
 endif()
 
 if (OPENCV_CORE_FOUND)
-    message(STATUS "Building with OpenCV support.")
+    message(STATUS "Building with OpenCV support (${OpenCV_INSTALL_PATH}; version ${OpenCV_VERSION}).")
 else()
-    message(STATUS "Building without OpenCV support.")
+    message(STATUS "Building WITHOUT OpenCV support${selene_fp_failure_msg}.")
 endif()
 
--- docs/building.md
+++ docs/building.md
@@ -136,10 +136,10 @@ By default, the **Selene** CMake build instructions will automatically detect li
 present on the system and make use of these.
 If desired, this can be explicitly disabled by one or more of the following CMake options:
 
-    -DSELENE_NO_LIBJPEG=ON
-    -DSELENE_NO_LIBPNG=ON
-    -DSELENE_NO_LIBTIFF=ON
-    -DSELENE_NO_OPENCV=ON
+    -DSELENE_WITH_LIBJPEG=OFF
+    -DSELENE_WITH_LIBPNG=OFF
+    -DSELENE_WITH_LIBTIFF=OFF
+    -DSELENE_WITH_OPENCV=OFF
 
 The respective functionality, i.e. image I/O, or interoperability with OpenCV's `cv::Mat`, will then be disabled.
 
