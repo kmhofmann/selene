c381f5aa6185d0dcb55b4b360eebbeb6e01de43a
Michael Hofmann <kmhofmann@gmail.com>
2019-01-19 14:01:19 +0100

    Some version number changes for v0.3 release.

--- package/conan/conanfile.py
+++ package/conan/conanfile.py
@@ -1,10 +1,9 @@
-import os
 from conans import ConanFile, CMake
 
 
 class SeleneConan(ConanFile):
     name = "selene"
-    version = "0.2"
+    version = "0.3"
     license = "MIT"
     url = "https://github.com/kmhofmann/selene"
     description = "A C++17 image representation, processing and I/O library."
@@ -15,9 +14,9 @@ class SeleneConan(ConanFile):
     generators = "cmake"
     build_policy = "missing"
 
-    requires = ("libjpeg-turbo/[>1.5.0]@bincrafters/stable",
-                "libpng/[>1.2.0]@bincrafters/stable",
-                "libtiff/[>4.0.9]@bincrafters/stable")
+    requires = ("libjpeg-turbo/[>=1.5.0]@bincrafters/stable",
+                "libpng/[>=1.2.0]@bincrafters/stable",
+                "libtiff/[>=4.0.9]@bincrafters/stable")
 
     exports_sources = ("../../selene*",
                        "../../cmake*",
@@ -35,4 +34,12 @@ class SeleneConan(ConanFile):
         self.copy("license*", dst="licenses",  ignore_case=True, keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["selene_base", "selene_img", "selene_img_io", "selene_img_ops", "selene_io"]
+        self.cpp_info.libs = ["selene_base",
+                              "selene_base_io",
+                              "selene_img",
+                              "selene_img_io",
+                              "selene_img_io_jpeg",
+                              "selene_img_io_png",
+                              "selene_img_io_tiff",
+                              "selene_img_ops",
+                              "selene_io"]
