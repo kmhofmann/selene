34d6653b57b0fe6f1222304df8d9c7cf35681576
Michael Hofmann <kmhofmann@gmail.com>
2019-02-20 21:45:35 +0100

    More explicit convolution code for better code generation.

--- selene/img_ops/Convolution.hpp
+++ selene/img_ops/Convolution.hpp
@@ -58,16 +58,27 @@ namespace impl {
 template <typename ConvolutionResultType, BorderAccessMode access_mode, typename DerivedSrc,
           typename KernelValueType, KernelSize kernel_size>
 inline auto convolve_pixels_x(const ImageBase<DerivedSrc>& img_src, PixelIndex x, PixelIndex y,
-                       const Kernel<KernelValueType, kernel_size>& kernel,
-                       PixelIndex::value_type k_offset)
+                              const Kernel<KernelValueType, kernel_size>& kernel,
+                              PixelIndex::value_type k_offset)
 {
   auto sum = PixelTraits<ConvolutionResultType>::zero_element;
 
-  for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+  if constexpr (access_mode == BorderAccessMode::Unchecked)
   {
-    const auto x_idx = PixelIndex{x + static_cast<PixelIndex::value_type>(k_idx) - k_offset};
-    const auto& px = ImageBorderAccessor<access_mode>::access(img_src, x_idx, y);
-    sum += kernel[k_idx] * px;
+    auto* ptr = img_src.data(x - k_offset, y);
+    for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+    {
+      sum += kernel[k_idx] * *ptr++;
+    }
+  }
+  else
+  {
+    auto x_idx = PixelIndex{x - k_offset};
+    for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+    {
+      const auto& px = ImageBorderAccessor<access_mode>::access(img_src, x_idx++, y);
+      sum += kernel[k_idx] * px;
+    }
   }
 
   return sum;
@@ -76,16 +87,31 @@ inline auto convolve_pixels_x(const ImageBase<DerivedSrc>& img_src, PixelIndex x
 template <typename ConvolutionResultType, BorderAccessMode access_mode, typename DerivedSrc,
     typename KernelValueType, KernelSize kernel_size>
 inline auto convolve_pixels_y(const ImageBase<DerivedSrc>& img_src, PixelIndex x, PixelIndex y,
-                       const Kernel<KernelValueType, kernel_size>& kernel,
-                       PixelIndex::value_type k_offset)
+                              const Kernel<KernelValueType, kernel_size>& kernel,
+                              PixelIndex::value_type k_offset)
 {
   auto sum = PixelTraits<ConvolutionResultType>::zero_element;
 
-  for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+//  using PixelType = typename DerivedSrc::PixelType;
+//  if constexpr (access_mode == BorderAccessMode::Unchecked)
+//  {
+//    const auto row_offset = img_src.row_bytes();
+//    auto ptr = img_src.data(x, y - k_offset);
+//    auto ptr_b = reinterpret_cast<const unsigned char*>(ptr);
+//    for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+//    {
+//      sum += kernel[k_idx] * *(reinterpret_cast<const PixelType*>(ptr_b));
+//      ptr_b += row_offset;
+//    }
+//  }
+//  else
   {
-    const auto y_idx = PixelIndex{y + static_cast<PixelIndex::value_type>(k_idx) - k_offset};
-    const auto& px = ImageBorderAccessor<access_mode>::access(img_src, x, y_idx);
-    sum += kernel[k_idx] * px;
+    auto y_idx = PixelIndex{y - k_offset};
+    for (auto k_idx = std::size_t{0}; k_idx < kernel.size(); ++k_idx)
+    {
+      const auto& px = ImageBorderAccessor<access_mode>::access(img_src, x, y_idx++);
+      sum += kernel[k_idx] * px;
+    }
   }
 
   return sum;
@@ -131,37 +157,38 @@ void convolution_x(const ImageBase<DerivedSrc>& img_src, ImageBase<DerivedDst>& 
   const auto x_left = k_offset;
   const auto x_right = img_src.width() - k_offset;
 
-  auto write_to_dst = [&img_dst](auto res, auto x, auto y) {
+  auto write_to_dst = [](auto res, auto* ptr) {
     if constexpr (/*std::is_integral_v<ConvolutionResultType> &&*/ shift_right > 0)
     {
-      img_dst(x, y) = (res + (1 << (shift_right - 1))) >> shift_right;
+      *ptr = (res + (1 << (shift_right - 1))) >> shift_right;
     }
     else
     {
-      img_dst(x, y) = sln::round<ElementTypeDst>(res);
+      *ptr = sln::round<ElementTypeDst>(res);
     }
   };
 
   for (auto y = 0_idx; y < img_dst.height(); ++y)
   {
     auto x = 0_idx;
+    auto* ptr_dst = img_dst.data(y);
 
     for (; x < x_left; ++x)
     {
       const auto res = impl::convolve_pixels_x<ConvolutionResultType, access_mode>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
 
     for (; x < x_right; ++x)
     {
       const auto res = impl::convolve_pixels_x<ConvolutionResultType, BorderAccessMode::Unchecked>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
 
     for (; x < img_dst.width(); ++x)
     {
       const auto res = impl::convolve_pixels_x<ConvolutionResultType, access_mode>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
   }
 }
@@ -225,43 +252,50 @@ void convolution_y(const ImageBase<DerivedSrc>& img_src, ImageBase<DerivedDst>& 
   const auto y_top = k_offset;
   const auto y_bottom = img_src.height() - k_offset;
 
-  auto write_to_dst = [&img_dst](auto res, auto x, auto y) {
+  auto write_to_dst = [](auto res, auto* ptr) {
     if constexpr (/*std::is_integral_v<ConvolutionResultType> &&*/ shift_right > 0)
     {
-      img_dst(x, y) = (res + (1 << (shift_right - 1))) >> shift_right;
+      *ptr = (res + (1 << (shift_right - 1))) >> shift_right;
     }
     else
     {
-      img_dst(x, y) = sln::round<ElementTypeDst>(res);
+      *ptr = sln::round<ElementTypeDst>(res);
     }
   };
 
   auto y = 0_idx;
 
+
   for (; y < y_top; ++y)
   {
+    auto* ptr_dst = img_dst.data(y);
+
     for (auto x = 0_idx; x < img_dst.width(); ++x)
     {
       const auto res = impl::convolve_pixels_y<ConvolutionResultType, access_mode>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
   }
 
   for (; y < y_bottom; ++y)
   {
+    auto* ptr_dst = img_dst.data(y);
+
     for (auto x = 0_idx; x < img_dst.width(); ++x)
     {
       const auto res = impl::convolve_pixels_y<ConvolutionResultType, BorderAccessMode::Unchecked>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
   }
 
   for (; y < img_dst.height(); ++y)
   {
+    auto* ptr_dst = img_dst.data(y);
+
     for (auto x = 0_idx; x < img_dst.width(); ++x)
     {
       const auto res = impl::convolve_pixels_y<ConvolutionResultType, access_mode>(img_src, x, y, kernel, k_offset);
-      write_to_dst(res, x, y);
+      write_to_dst(res, ptr_dst++);
     }
   }
 }
